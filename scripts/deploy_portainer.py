8e7f6cf47ca7d86222659ed72c5c7f4b924b7b51
Bram Kelchtermans <info@bramkelchtermans.be>
2024-07-31 23:01:01 +0200

    Refactor deploy_portainer.py to include repository information in stack creation

--- scripts/deploy_portainer.py
+++ scripts/deploy_portainer.py
@@ -2,6 +2,7 @@ import os
 import sys
 import json
 import requests
+import uuid
 
 def get_environment_id(environment_name, environment_map):
     env_map = json.loads(environment_map)
@@ -14,22 +15,27 @@ def get_stacks(portainer_url, api_key, environment_id):
     response = requests.get(f'{portainer_url}/api/stacks', headers=headers, params={'filters': json.dumps({'EnvironmentId': environment_id})}, verify=False)
     return response.json()
 
-def create_stack(portainer_url, api_key, environment_id, stack_name, compose_file_path):
+def create_stack(portainer_url, api_key, environment_id, stack_name, compose_file_path, repository_url, repository_username, repository_password):
     headers = {
         'X-API-Key': f'{api_key}',
         'Content-Type': 'application/json'
     }
-    with open(compose_file_path, 'r') as file:
-        compose_content = file.read()
-    
+    randUuid = uuid.uuid4()
+
     data = {
-        'Name': stack_name,
-        'SwarmID': '',
-        'StackFileContent': compose_content,
-        'Env': [],
-        'Prune': False,
-    }
-    response = requests.post(f'{portainer_url}/api/stacks', headers=headers, json=data, params={'type': 2, 'method': 'string', 'endpointId': environment_id}, verify=False)
+        "name": stack_name,
+        "repositoryURL": repository_url,
+        "repositoryUsername": repository_username,
+        "repositoryPassword": repository_password,
+        "repositoryAuthentication": True,
+        "composeFile": compose_file_path,
+        "autoUpdate": 
+        {
+                "webhook": randUuid
+            }
+        }
+
+    response = requests.post(f'{portainer_url}/api/stacks?type=2&method=repository&endpointId=' + environment_id, headers=headers, json=data, params={'type': 2, 'method': 'string', 'endpointId': environment_id}, verify=False)
     return response.status_code, response.json()
 
 def update_stack(portainer_url, webhook_uuid):
@@ -49,6 +55,10 @@ def main():
     api_key = sys.argv[2]
     environment_map = sys.argv[3]
     changed_files_path = sys.argv[4]
+    
+    repository_url = sys.argv[5]
+    repository_username = sys.argv[6]
+    repository_password = sys.argv[7]
 
     print("portainer_url:", portainer_url)
     print("api_key:", api_key)
@@ -92,7 +102,7 @@ def main():
                     print(f"Updated stack {stack_name} in environment {environment_name}. Status code: {status_code}. Response: {response}")
                 else:
                     # Stack does not exist, create it
-                    status_code, response = create_stack(portainer_url, api_key, environment_id, stack_name, file_path)
+                    status_code, response = create_stack(portainer_url, api_key, environment_id, stack_name, file_path, repository_url, repository_username, repository_password)
                     print(f"Created stack {stack_name} in environment {environment_name}. Status code: {status_code}. Response: {response}")
 
 if __name__ == "__main__":
